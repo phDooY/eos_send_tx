3be8dc23147ab288b8b0706fad4b8c47693995cb
phDooY <david91.05@mail.ru>
2018-11-14 00:55:57 +0300

    example tx sending 1 EOS to chosen account

---
+++ send_tx.py
@@ -0,0 +1,47 @@
+from config import *
+
+import eospy.keys
+import eospy.types
+import eospy.cleos
+
+#ce = eospy.cleos.Cleos(url='http://api.pennstation.eosnewyork.io:7001')
+ce = eospy.cleos.Cleos(url='https://api.eosdetroit.io:443')
+
+# to check if chain is active
+#ce.get_chain_lib_info()
+
+key1 = eospy.keys.EOSKey(ACC_PK)
+
+memo_txt = "EOSpy sucks! Mhahahahah"
+
+payload = [
+        {
+            'args': {
+                "from": ACC_NAME_FROM,  # sender
+                "to": ACC_NAME_TO,  # receiver
+                "quantity": '1.0000 EOS',  # In EOS
+                "memo": memo_txt,
+            },
+            "account": "eosio.token",
+            "name": "transfer",
+            "authorization": [{
+                "actor": ACC_NAME_FROM,
+                "permission": "active",
+            }],
+        }
+    ]
+
+
+data=ce.abi_json_to_bin(payload[0]['account'],payload[0]['name'],payload[0]['args'])
+
+payload[0]['data']=data['binargs']
+
+payload[0].pop('args')
+
+trx = {"actions":[payload[0]]}
+
+tx1 = ce.push_transaction(trx, [key1])
+
+print('------------------------------------------------')
+print(tx1)
+print('------------------------------------------------')
